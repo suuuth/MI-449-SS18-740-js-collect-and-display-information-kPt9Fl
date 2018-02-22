590b56e66781106cfb681cbbcd8b12c50057b21b
suuuth <suther98@msu.edu>
2018-02-22 14:41:17 -0500

    fixed linting errors, used textContent, reduced duplicate strings, changed function name

--- js-collect-display-info.js
+++ js-collect-display-info.js
@@ -7,36 +7,29 @@ var telInput = document.getElementById('tel')
 var textOutput = document.getElementById('result')
 var codeOutput = document.getElementById('code-result')
 
-var update = function () {
+
+var updateProfile = function () {
   var firstName = firstNameInput.value
   var lastName = lastNameInput.value
   var descrip = descripInput.value
   var email = emailInput.value
   var tel = telInput.value
 
-  textOutput.innerHTML = 
-    '<h4>Hi, my name is ' + firstName + '&nbsp;' + lastName + '</h4>' +
+  var profile = '<h4>Hi, my name is ' + firstName + '&nbsp;' + lastName + '</h4>' +
     '<p>' + descrip + '</p>' +
     '<p>' +
-      'If you\'re interested in a date, you can email me at ' +
-      '<a href="mailto:' + email + 'target="_blank">' + email + '</a>' +
-      ' or give me a call at ' +
-      '<a href="tel:' + tel + '" target="_blank">' + tel + '</a>' +
-    '</p>';
+    'If you\'re interested in a date, you can email me at ' +
+    '<a href="mailto:' + email + 'target="_blank">' + email + '</a>' +
+    ' or give me a call at ' +
+    '<a href="tel:' + tel + '" target="_blank">' + tel + '</a>' +
+    '</p>'
 
-  codeOutput.innerHTML =
-    '&lth4&gt;Hi, my name is ' + firstName + '&nbsp;' + lastName + '&lt/h4&gt;' +
-    '&ltp&gt;' + descrip + '&lt/p&gt;' +
-    '&ltp&gt;' +
-      'If you\'re interested in a date, you can email me at ' +
-      '&lta href="mailto:' + email + ' target="_blank"&gt;' + email + '&lt/a&gt;' +
-      ' or give me a call at ' +
-      '&lta href="tel:' + tel + '" target="_blank"&gt;' + tel + '&lt/a&gt;' +
-    '&lt/p&gt;'
+  textOutput.innerHTML = profile
+  codeOutput.textContent = profile
 }
 
-firstNameInput.addEventListener('input', update)
-lastNameInput.addEventListener('input', update)
-descripInput.addEventListener('input', update)
-emailInput.addEventListener('input', update)
-telInput.addEventListener('input', update)
+firstNameInput.addEventListener('input', updateProfile)
+lastNameInput.addEventListener('input', updateProfile)
+descripInput.addEventListener('input', updateProfile)
+emailInput.addEventListener('input', updateProfile)
+telInput.addEventListener('input', updateProfile)
